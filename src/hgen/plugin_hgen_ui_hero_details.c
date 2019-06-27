e96d54c516a138069f69d7c30e4196b224d45c91
bug00r <bachus@gmxnet>
2019-06-27 15:28:27 +0200

    add hero properties

--- src/hgen/plugin_hgen_ui_hero_details.c
+++ src/hgen/plugin_hgen_ui_hero_details.c
@@ -224,14 +224,22 @@ static void __hgen_on_change_eye_col_dice(Ihandle *ih) {
 }
 
 
-#if 0
-//############################################################################################################################
-//EOF private SEction
-//############################################################################################################################
-#endif
+static void __hgen_details_set_baseinfo_gbox_atts(Ihandle *gbox) {
 
-Ihandle* hgen_hero_sheet_new(dsa_heros_t *heros, dsa_hero_t *hero) {
+	IupSetAttribute(gbox,"ORIENTATION","HORIZONTAL");
+	IupSetAttribute(gbox,"NUMDIV","3");
+	IupSetAttribute(gbox,"EXPAND","NO");
+	IupSetAttribute(gbox, "SIZELIN", "-1");
+	IupSetAttribute(gbox, "SIZECOL", "-1");
+	IupSetAttribute(gbox, "GAPLIN", "5");
+	IupSetAttribute(gbox, "GAPCOL", "5");
+	IupSetAttribute(gbox, "MARGIN", "5x5");
+	IupSetAttribute(gbox,"ALIGNMENTLIN","ACENTER");
+	IupSetAttribute(gbox,"ALIGNMENTCOL","ALEFT");
+}
 
+static Ihandle* __hgen_details_gen_baseinfo(Ihandle *detail_frame, dsa_heros_t *heros, dsa_hero_t *hero) {
+	
 	Ihandle *lbl_gp = IupLabel("GP:");
 
 	Ihandle *txt_gp = IupText(NULL);
@@ -337,38 +345,162 @@ Ihandle* hgen_hero_sheet_new(dsa_heros_t *heros, dsa_hero_t *hero) {
 		lbl_eye_col, lst_eye_col, btn_edt_eye_col,
 	NULL);
 
-	IupSetAttribute(gbox,"ORIENTATION","HORIZONTAL");
-	IupSetAttribute(gbox,"NUMDIV","3");
-	IupSetAttribute(gbox,"EXPAND","NO");
-	IupSetAttribute(gbox, "SIZELIN", "-1");
-	IupSetAttribute(gbox, "SIZECOL", "-1");
-	IupSetAttribute(gbox, "GAPLIN", "5");
-	IupSetAttribute(gbox, "GAPCOL", "5");
-	IupSetAttribute(gbox, "MARGIN", "5x5");
-	IupSetAttribute(gbox,"ALIGNMENTLIN","ACENTER");
-	IupSetAttribute(gbox,"ALIGNMENTCOL","ALEFT");
+	__hgen_details_set_baseinfo_gbox_atts(gbox);
 
-    Ihandle *detail_frame = IupHbox(gbox, NULL);
-
-    IupSetAttribute(detail_frame, "culture_list", (void*)lst_culture);
+	IupSetAttribute(detail_frame, "culture_list", (void*)lst_culture);
     IupSetAttribute(detail_frame, "breed_list", (void*)lst_breed);
     IupSetAttribute(detail_frame, "prof_list", (void*)lst_prof);
     IupSetAttribute(detail_frame, "name_txt", (void*)txt_name);
 	IupSetAttribute(detail_frame, "gp_txt", (void*)txt_gp);
+
+	return gbox;
+}
+
+static int __hgen_on_change_attr(Ihandle *ih, int pos) {
+	
+	int old_val = IupGetInt(ih, "VALUE");
+
+	DEBUG_LOG_ARGS("attr: old %i new %i\n", old_val, pos);
+
+	if ( old_val != pos ) {
+		
+		dsa_hero_t *hero = (dsa_hero_t *)IupGetAttribute(ih, "hero");
+		
+		const unsigned char* short_name = (const unsigned char*)IupGetAttribute(ih, "short_name");
+		
+		if ( (pos - old_val) > 0 ) {
+			dsa_heros_attr_inc(hero, short_name);
+		} else {
+			dsa_heros_attr_dec(hero, short_name);
+		}
+
+		xmlChar * real = dsa_heros_get_attr(hero, short_name);
+
+		DEBUG_LOG_ARGS("attr %s after edit %s\n", short_name, real);
+
+		IupSetStrAttribute(ih, "VALUE", real);
+
+		xmlFree(real);
+	}
+
+	return IUP_DEFAULT;
+}
+
+static Ihandle* __hgen_details_gen_attr_field(dsa_hero_t *hero, const char* short_name) {
+	Ihandle *field = IupText(NULL);
+	IupSetAttributes(field,"RASTERSIZE=40, MULTILINE=NO, SPIN=yes, SPINAUTO=no");
+    IupSetAttribute(field, "hero", (void*)hero);
+	IupSetAttribute(field, "short_name", (void*)short_name);
+	IupSetCallback(field,"SPIN_CB",(Icallback)__hgen_on_change_attr);
+	return field;
+}
+
+static Ihandle* __hgen_details_gen_properties(Ihandle *detail_frame, dsa_heros_t *heros, dsa_hero_t *hero) {
+	
+	Ihandle *lbl_mu = IupLabel("MU:");
+	Ihandle *txt_mu = __hgen_details_gen_attr_field(hero, "MU");
+	IupSetAttribute(detail_frame, "txt_mu", (void*)txt_mu);
+
+	Ihandle *lbl_kl = IupLabel("KL:");
+	Ihandle *txt_kl = __hgen_details_gen_attr_field(hero, "KL");
+	IupSetAttribute(detail_frame, "txt_kl", (void*)txt_kl);
+
+	Ihandle *lbl_kk = IupLabel("KK:");
+	Ihandle *txt_kk = __hgen_details_gen_attr_field(hero, "KK");
+	IupSetAttribute(detail_frame, "txt_kk", (void*)txt_kk);
+
+	Ihandle *lbl_ge = IupLabel("GE:");
+	Ihandle *txt_ge = __hgen_details_gen_attr_field(hero, "GE");
+	IupSetAttribute(detail_frame, "txt_ge", (void*)txt_ge);
+
+	Ihandle *lbl_ff = IupLabel("FF:");
+	Ihandle *txt_ff = __hgen_details_gen_attr_field(hero, "FF");
+	IupSetAttribute(detail_frame, "txt_ff", (void*)txt_ff);
+
+	Ihandle *lbl_in = IupLabel("IN:");
+	Ihandle *txt_in = __hgen_details_gen_attr_field(hero, "IN");
+	IupSetAttribute(detail_frame, "txt_in", (void*)txt_in);
+
+	Ihandle *lbl_ko = IupLabel("KO:");
+	Ihandle *txt_ko = __hgen_details_gen_attr_field(hero, "KO");
+	IupSetAttribute(detail_frame, "txt_ko", (void*)txt_ko);
+
+	Ihandle *lbl_ch = IupLabel("CH:");
+	Ihandle *txt_ch = __hgen_details_gen_attr_field(hero, "CH");
+	IupSetAttribute(detail_frame, "txt_ch", (void*)txt_ch);
+
+	Ihandle *lbl_so = IupLabel("SO:");
+	Ihandle *txt_so = __hgen_details_gen_attr_field(hero, "SO");
+	IupSetAttribute(detail_frame, "txt_so", (void*)txt_so);
+
+	Ihandle *gbox = IupGridBox(
+		lbl_mu, txt_mu,
+		lbl_kl, txt_kl,
+		lbl_in, txt_in,
+		lbl_ch, txt_ch,
+		lbl_ff, txt_ff,
+		lbl_ge, txt_ge,
+		lbl_ko, txt_ko,
+		lbl_kk, txt_kk,
+		lbl_so, txt_so,
+	NULL);
+
+	__hgen_details_set_baseinfo_gbox_atts(gbox);
+
+	IupSetAttribute(gbox,"NUMDIV","2");
+	IupSetAttribute(gbox, "GAPLIN", "6");
+
+	return gbox;
+}
+
+static void __hgen_details_fill_prop(Ihandle *sheet, dsa_hero_t *hero, const char *handle_name, const unsigned char *short_name) {
+	xmlChar * val = dsa_heros_get_attr(hero, short_name);
+	IupSetStrAttribute((Ihandle *)IupGetAttribute(sheet, handle_name), "VALUE", (const char*) val);
+    IupSetStrAttribute((Ihandle *)IupGetAttribute(sheet, handle_name), "SPINVALUE", (const char*) val);
+    xmlFree(val);
+}
+
+#if 0
+//############################################################################################################################
+//EOF private SEction
+//############################################################################################################################
+#endif
+
+Ihandle* hgen_hero_sheet_new(dsa_heros_t *heros, dsa_hero_t *hero) {
+
+	Ihandle *detail_frame = IupVbox(NULL);
+
+	Ihandle *bi_basic = __hgen_details_gen_baseinfo(detail_frame, heros, hero);
+
+	Ihandle *bi_props = __hgen_details_gen_properties(detail_frame, heros, hero);
+
+	Ihandle *baseinfo = IupHbox(bi_basic, bi_props, NULL);
+
+    IupAppend(detail_frame, baseinfo);
 
 	return detail_frame;
 }
 
 void hgen_hero_sheet_init(Ihandle *sheet, dsa_heros_t *heros, dsa_hero_t *hero) {
 
-    xmlChar * val =  dsa_heros_get_name(hero);
+    xmlChar * val = dsa_heros_get_name(hero);
     IupSetStrAttribute((Ihandle *)IupGetAttribute(sheet, "name_txt"), "VALUE", (const char*) val);
     xmlFree(val);
 
-	val =  dsa_heros_get_gp(hero);
+	val = dsa_heros_get_gp(hero);
 	IupSetStrAttribute((Ihandle *)IupGetAttribute(sheet, "gp_txt"), "VALUE", (const char*) val);
     IupSetStrAttribute((Ihandle *)IupGetAttribute(sheet, "gp_txt"), "SPINVALUE", (const char*) val);
     xmlFree(val);
+
+	__hgen_details_fill_prop(sheet, hero, "txt_mu", (const unsigned char*)"MU");
+	__hgen_details_fill_prop(sheet, hero, "txt_kl", (const unsigned char*)"KL");
+	__hgen_details_fill_prop(sheet, hero, "txt_in", (const unsigned char*)"IN");
+	__hgen_details_fill_prop(sheet, hero, "txt_ch", (const unsigned char*)"CH");
+	__hgen_details_fill_prop(sheet, hero, "txt_ff", (const unsigned char*)"FF");
+	__hgen_details_fill_prop(sheet, hero, "txt_ge", (const unsigned char*)"GE");
+	__hgen_details_fill_prop(sheet, hero, "txt_ko", (const unsigned char*)"KO");
+	__hgen_details_fill_prop(sheet, hero, "txt_kk", (const unsigned char*)"KK");
+	__hgen_details_fill_prop(sheet, hero, "txt_so", (const unsigned char*)"SO");
 
     init_breeds((Ihandle *)IupGetAttribute(sheet, "breed_list"), heros);
 	init_cultures((Ihandle *)IupGetAttribute(sheet, "culture_list"), heros);
